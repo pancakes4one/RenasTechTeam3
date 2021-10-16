04bb2f7e8d46a165c72a5974c25f684b90cda4f7
Jessicca <jessicca@Q1452183.hitronhub.home>
2021-10-16 10:38:54 -0400

    homework added to branch

---
+++ src/Jessicca/homework/Book.java
@@ -0,0 +1,25 @@
+package Jessicca.homework;
+
+public class Book {
+
+    /*
+task1
+create 3 class  book ,  Audiobook  ,PaperBook
+    tostring()
+ */
+
+        public String title;
+        public String author;
+        public double price;
+        public int pages;
+
+        public String toString() {
+                return "Book" + "\n" + "{ " +
+                        "Title = " + title + "\n" +
+                        "Author = " + author + "\n" +
+                        "Price = $" + price + "\n" +
+                        "Length = " + pages + " pages }" ;
+        }
+
+
+}
